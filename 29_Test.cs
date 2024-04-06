61eca00de2e6f79863b8a306f3bbfd0617aa5c4d
MongLick <qkjm4836@gmail.com>
2024-04-06 22:36:48 +0900

    feat : 문자 반복 출력하기

---
+++ 29_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution29
+{
+	public string solution(string my_string, int n)
+	{
+		string answer = "";
+		char a;
+
+		for (int i = 0; i < my_string.Length; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				a = my_string[i];
+				answer += a.ToString();
+			}
+		}
+
+		return answer;
+	}
+}
