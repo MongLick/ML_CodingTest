7af5e644a70dc28929a43c53a1158178ae3adf7b
MongLick <qkjm4836@gmail.com>
2024-04-06 22:40:55 +0900

    feat : 짝수는 싫어요

---
+++ 35_Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Solution35
+{
+	public int[] solution(int n)
+	{
+		int a = 0;
+		if (n % 2 == 0)
+		{
+			a = n / 2;
+		}
+		else if (n % 2 != 0)
+		{
+			a = (n + 1) / 2;
+		}
+		int[] answer = new int[a];
+
+		a = 0;
+
+		for (int i = 1; i <= n; i++)
+		{
+			if (i % 2 != 0)
+			{
+				answer[a] = i;
+				a++;
+			}
+		}
+		return answer;
+	}
+}
