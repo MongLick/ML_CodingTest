46d0b027e48f2959b4f115574e9ff48ec5c26ba7
MongLick <qkjm4836@gmail.com>
2024-04-06 22:24:05 +0900

    feat : 짝수 홀수 개수

---
+++ 16_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Solution16
+{
+	public int[] solution(int[] num_list)
+	{
+		int[] answer = new int[2];
+
+		foreach (int a in num_list)
+		{
+			if (a % 2 == 0)
+			{
+				answer[0]++;
+			}
+			else
+			{
+				answer[1]++;
+			}
+		}
+
+		return answer;
+	}
+}
