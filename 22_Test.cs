af7e0fde6caeda9d3c29c3359f099f89a5408db4
MongLick <qkjm4836@gmail.com>
2024-04-06 22:28:43 +0900

    feat : 최대값 만들기(1)

---
+++ 22_Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+public class Solution22
+{
+	public int solution(int[] numbers)
+	{
+		int answer = 0;
+		Array.Sort(numbers);
+		Array.Reverse(numbers);
+		answer = numbers[0] * numbers[1];
+		return answer;
+	}
+}
