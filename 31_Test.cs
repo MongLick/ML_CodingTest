8a7460cb4186ca9364e8c23bcf69712bdaf28346
MongLick <qkjm4836@gmail.com>
2024-04-06 22:38:32 +0900

    feat : 모음 제거

---
+++ 31_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Solution31
+{
+	public string solution(string my_string)
+	{
+		string answer = "";
+		string subStringList = "[aeiou]";
+		answer = Regex.Replace(my_string, subStringList, "");
+
+		return answer;
+	}
+}
