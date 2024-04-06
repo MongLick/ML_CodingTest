c4cb509966e00b11ebbaa1e19ecded74d0ee0ee5
MongLick <qkjm4836@gmail.com>
2024-04-06 22:30:09 +0900

    feat : 머쓱이보다 키 큰 사람

---
+++ 25_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution25
+{
+	public int solution(int[] array, int height)
+	{
+		int answer = 0;
+		for (int i = 0; i < array.Length; i++)
+		{
+			if (array[i] > height)
+			{
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
