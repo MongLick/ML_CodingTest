2b8cdc63e78c1a5b08eb991d316228521fcac202
MongLick <qkjm4836@gmail.com>
2024-04-06 22:39:32 +0900

    feat : 피자 나눠 먹기(3)

---
+++ 33_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution33
+{
+	public int solution(int slice, int n)
+	{
+		int answer = 0;
+		double a;
+
+		a = (double)n / slice;
+		a = Math.Ceiling(a);
+		answer = (int)a;
+		return answer;
+
+	}
+}
