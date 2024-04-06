bc0e1240bb12d26d87b5f7ec0541d5e57da8ef21
MongLick <qkjm4836@gmail.com>
2024-04-06 22:22:42 +0900

    feat : 배열 뒤집기

---
+++ 13_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution13
+{
+	public List<int> solution(int[] num_list)
+	{
+
+		Stack<int> s = new Stack<int>();
+		List<int> list = new List<int>();
+
+		foreach (int a in num_list)
+		{
+			s.Push(a);
+		}
+
+		while (s.Count > 0)
+		{
+			list.Add(s.Pop());
+		}
+
+		return list;
+	}
+}
