4be567136765c7007b5c5ee90d926470f576f7b3
MongLick <qkjm4836@gmail.com>
2024-04-06 22:39:12 +0900

    feat : 제곱수 판별하기

---
+++ 32_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution32
+{
+	public int solution(int n)
+	{
+		int answer = 0;
+
+		for (int i = 0; i <= n; i++)
+		{
+			if (i * i == n)
+			{
+				answer = 1;
+				break;
+			}
+			else
+			{
+				answer = 2;
+			}
+		}
+
+		return answer;
+	}
+}
