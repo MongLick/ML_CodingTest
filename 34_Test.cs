39d8a6ec11dfd713c36a6b1a6afc0856a0b0989d
MongLick <qkjm4836@gmail.com>
2024-04-06 22:40:15 +0900

    feat : 피자 나눠 먹기(1)

---
+++ 34_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution34
+{
+	public int solution(int n)
+	{
+		int answer = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			if (i <= 7)
+			{
+				answer = 1;
+			}
+			else if (i % 7 == 0)
+			{
+				answer++;
+			}
+		}
+		if (n % 7 >= 1 && n > 7)
+		{
+			answer++;
+		}
+		return answer;
+	}
+}
