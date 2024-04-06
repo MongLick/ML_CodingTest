cecdad2f94c0f6d7fddfe59dacea3e68f3cc9240
MongLick <qkjm4836@gmail.com>
2024-04-06 22:42:58 +0900

    feat : 세균 증식

---
+++ 37_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution37
+{
+	public int solution(int n, int t)
+	{
+		int answer = 0;
+
+		for (int i = 1; i <= t; i++)
+		{
+			n = n * 2;
+		}
+
+		answer += n;
+
+		return answer;
+	}
+}
