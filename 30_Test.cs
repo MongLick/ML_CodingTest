6a684b5f2ec731a8726093594ff7ef7f723fc329
MongLick <qkjm4836@gmail.com>
2024-04-06 22:38:23 +0900

    feat : 순서쌍의 개수

---
+++ 30_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution30
+{
+	public int solution(int n)
+	{
+		int answer = 0;
+
+		for (int i = 1; i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
