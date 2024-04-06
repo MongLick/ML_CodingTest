f6142faf1ca8b4cbd5eb3004ab52f575e02c4f5d
MongLick <qkjm4836@gmail.com>
2024-04-06 22:42:32 +0900

    feat : 삼각형의 완성조건(1)

---
+++ 36_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public class Solution36
+{
+	public int solution(int[] sides)
+	{
+		int answer = 0;
+
+		int max = sides.Max();
+		Array.Sort(sides);
+
+		if (sides[0] + sides[1] > max)
+		{
+			answer = 1;
+		}
+		else
+		{
+			answer = 2;
+		}
+
+		return answer;
+	}
+}
