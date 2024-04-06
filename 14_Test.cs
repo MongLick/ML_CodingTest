243ee5433e74386ec867e1c07752187689b8a0c3
MongLick <qkjm4836@gmail.com>
2024-04-06 22:23:14 +0900

    feat : 배열 원소의 길이

---
+++ 14_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+public class Solution14
+{
+	public int[] solution(string[] strlist)
+	{
+		List<int> list = new List<int>();
+		foreach (string element in strlist)
+		{
+			list.Add(element.Length);
+		}
+		return list.ToArray();
+	}
+}
