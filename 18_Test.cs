235d9d18f553081d6d847ab204f5335b6537546a
MongLick <qkjm4836@gmail.com>
2024-04-06 22:25:51 +0900

    feat : 양꼬치

---
+++ 18_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution18
+{
+	public int solution(int n, int k)
+	{
+		int answer = 0;
+		int a = 12000;
+		int b = 2000;
+
+		answer += a * n;
+		answer += b * k;
+
+		while (n > 9)
+		{
+			n -= 10;
+			answer -= b;
+		}
+		return answer;
+	}
+}
