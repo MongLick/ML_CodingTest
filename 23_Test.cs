299c343eda4821ef9f7fb2f7c27b42255adb3eef
MongLick <qkjm4836@gmail.com>
2024-04-06 22:29:00 +0900

    feat : 아이스 아메리카노

---
+++ 23_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution23
+{
+	public int[] solution(int money)
+	{
+		int[] answer = new int[2];
+		int a = 0;
+		while (money >= 5500)
+		{
+			money -= 5500;
+			a++;
+		}
+		answer[0] = a;
+		answer[1] = money;
+		return answer;
+	}
+}
