13b7fb04c200f296120be917b36ea7e1fe9f7aa8
MongLick <qkjm4836@gmail.com>
2024-04-06 22:43:28 +0900

    feat : 중앙값 구하기

---
+++ 39_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution39
+{
+	public int solution(int[] array)
+	{
+		int answer = 0;
+		int a;
+		Array.Sort(array);
+
+		a = array.Length / 2;
+		answer = array[a];
+
+		return answer;
+	}
+}
