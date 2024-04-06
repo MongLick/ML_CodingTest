6ace0a87bd61a8b53294da9e2ac671b3d51126a0
MongLick <qkjm4836@gmail.com>
2024-04-06 22:23:40 +0900

    feat : 중복된 숫자 개수

---
+++ 15_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution15
+{
+	public int solution(int[] array, int n)
+	{
+		int answer = 0;
+		int a;
+
+		for (int i = 0; i < array.Length; i++)
+		{
+			a = array[i];
+			if (a == n)
+			{
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+}
