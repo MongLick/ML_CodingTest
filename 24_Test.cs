9cb5b9c76bde935cfb2cff53d14ffc6a55cef006
MongLick <qkjm4836@gmail.com>
2024-04-06 22:29:17 +0900

    feat : 문자열안에 문자열

---
+++ 24_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution24
+{
+	public int solution(string str1, string str2)
+	{
+		int answer = 0;
+		if (str1.Contains(str2))
+		{
+			return answer = 1;
+		}
+		else
+		{
+			return answer = 2;
+		}
+		return answer;
+	}
+}
