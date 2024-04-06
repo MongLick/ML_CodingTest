265947f1eeb819ae005ddbcbfb9050d1dff9001d
MongLick <qkjm4836@gmail.com>
2024-04-06 22:27:25 +0900

    feat : 문자열 뒤집기

---
+++ 20_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class Solution20
+{
+	public string solution(string my_string)
+	{
+		string answer = "";
+
+		char[] charArr = my_string.ToArray();
+		Array.Reverse(charArr);
+		answer = new string(charArr);
+
+		return answer;
+	}
+}
